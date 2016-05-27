6d0bb056a0c49999a834d7f30cc558b98d991207
Jake Reynolds <jakealexis@gmail.com>
2016-05-27 12:00:24 +0100

    latest

--- Final Report/make.sh
+++ Final Report/make.sh
@@ -1,3 +1,9 @@
 
 
-pdflatex FinalReport.tex -output-directory=build && "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "C:\Users\Jake\meng\meng\Final Report\build\FinalReport.pdf"
+
+
+pdflatex FinalReport -output-directory=build && 
+biber FinalReport --input-directory=build --output-directory=build && 
+pdflatex FinalReport -output-directory=build && 
+pdflatex FinalReport -output-directory=build &&  
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "C:\Users\Jake\meng\meng\Final Report\build\FinalReport.pdf"
